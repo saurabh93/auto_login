95f5258bea8693e615583e09fe41a3a66b85977e
Saurabh <saurabh@saurabhcomp.com>
2016-01-24 02:31:55 +0530

    Minor improvement to prevent re-splitting in array

--- install.sh
+++ install.sh
@@ -31,9 +31,9 @@ check_command_exist()
 	test "$#" -eq 0 && echo "ERROR:${NO_PARAM}" && exit 1 #Checking Number of Arguments
 	
 	#Check commands exist or not
-	for inp in ${CMD_LIST[@]}
+	for inp in "${CMD_LIST[@]}"
 	do
-		[[ $(command -v $inp) ]]
+		[[ $(command -v "$inp") ]]
 		rc=$?;test "$rc" -eq 0 && status='OK' || status='Not Found'
 		echo -e	"$inp:	$rc" >> ${lib_path}/${conf}
 		echo -e	"            $inp: $status" 
@@ -46,7 +46,7 @@ check_run_time()
 	local count
 	
 	#Check configuration file,check for pre-requisites again & update ${conf}
-	for cmd in ${CMD_LIST[@]}
+	for cmd in "${CMD_LIST[@]}"
 	do
 		cmd_status=$(grep "$cmd" ${lib_path}/${conf} | awk '{print$2}')
 		if [[ "$cmd_status" -ne 0 ]];then
@@ -57,7 +57,7 @@ check_run_time()
 	done
 	
 	if [[ "$count" -ne 0 ]];then
-		cmd_status=$(grep 1 ${lib_path}/${conf} | awk '{print$1}' | tr -d '\n')
+		cmd_status="$(grep 1 ${lib_path}/${conf} | awk '{print$1}' | tr -d '\n')"
 		test ! -z "$cmd_status" && echo "Commands $cmd_status does not exist" && exit 1
 	fi
 	
@@ -66,7 +66,7 @@ check_run_time()
 check_files()
 {
 	egrep '.bashrc|profile' ${lib_path}/${conf} && return 0 	
-	for files in ${FILE_LSIT[@]}
+	for files in "${FILE_LSIT[@]}"
 	do
 		[[ -s ${HOME}/${files} ]]
 		rc=$?;test "$rc" -eq 0 && status='OK' || status='Not Found'
@@ -80,11 +80,7 @@ check_files()
 
 #Search prerequisites
 
-#if [[ ! $(uname -s) =~ (L|l)inux ]];then
-#	echo "Not a linux system.Exiting"
-#fi
-
-test -f ${lib_path}/${conf}  && check_run_time && check_files|| \
+{ test -f ${lib_path}/${conf}  && check_run_time && check_files; }|| \
 { mkdir -p ${lib_path} || exit ${DIR_PERM} && echo "Checking Pre-requisites" && check_command_exist bash expect gpg && check_files; } 
 
 for num in {0,1,2}

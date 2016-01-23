e4fa7155676812247e857704b3fadb9ea435165c
Saurabh <saurabh@saurabhcomp.com>
2016-01-24 01:47:05 +0530

    .bashrc/profile check

--- install.sh
+++ install.sh
@@ -15,11 +15,14 @@ clear #Clear screen
 HOME=${HOME:-$(pwd)}
 lib_path=${HOME}/.lib_auto
 conf='auto_login.conf'
+count=0
 
 #ERROR's
 NO_PARAM=67
-DIR_PERM=68
-
+NO_PERM=68
+NO_FILE=65
+CMD_LIST=(bash expect gpg)
+FILE_LSIT=(.bashrc .profile .bash_profile .gnupg/pubring.gpg .gnupg/secring.gpg)
 
 #Functions
 
@@ -28,7 +31,7 @@ check_command_exist()
 	test "$#" -eq 0 && echo "ERROR:${NO_PARAM}" && exit 1 #Checking Number of Arguments
 	
 	#Check commands exist or not
-	for inp in ${@}
+	for inp in ${CMD_LIST[@]}
 	do
 		[[ $(command -v $inp) ]]
 		rc=$?;test "$rc" -eq 0 && status='OK' || status='Not Found'
@@ -43,7 +46,7 @@ check_run_time()
 	local count
 	
 	#Check configuration file,check for pre-requisites again & update ${conf}
-	for cmd in {bash,expect,gpg}
+	for cmd in ${CMD_LIST[@]}
 	do
 		cmd_status=$(grep "$cmd" ${lib_path}/${conf} | awk '{print$2}')
 		if [[ "$cmd_status" -ne 0 ]];then
@@ -63,9 +66,9 @@ check_run_time()
 check_files()
 {
 	egrep '.bashrc|profile' ${lib_path}/${conf} && return 0 	
-	for files in {.bashrc,.profile,.bash_profile}
+	for files in ${FILE_LSIT[@]}
 	do
-		[[ -f ${HOME}/${files} ]]
+		[[ -s ${HOME}/${files} ]]
 		rc=$?;test "$rc" -eq 0 && status='OK' || status='Not Found'
                 echo -e "${files}:  $rc" >> ${lib_path}/${conf}
                 echo -e "            ${files}: $status" 
@@ -84,4 +87,12 @@ check_files()
 test -f ${lib_path}/${conf}  && check_run_time && check_files|| \
 { mkdir -p ${lib_path} || exit ${DIR_PERM} && echo "Checking Pre-requisites" && check_command_exist bash expect gpg && check_files; } 
 
-
+for num in {0,1,2}
+do
+	status=$(fgrep ${FILE_LSIT[$num]} ${lib_path}/${conf} | awk '{print$2}')
+	[[ "$status" -eq 1 ]] && count=$((count+1)) 
+	if [[ "$count" -eq 3 ]];then
+		{ [[ -f /etc/skel/${FILE_LSIT[0]} ]] && cp -v /etc/skel/${FILE_LSIT[0]} ${HOME}; } ||  touch ${HOME}/.bashrc || \
+		echo -e "${NO_PERM}" 
+	fi
+done
